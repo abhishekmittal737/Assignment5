5f75a8d4cf099427da5303d33b707d1023c2c624
abhishekmittal737 <56733216+abhishekmittal737@users.noreply.github.com>
2022-11-12 00:26:09 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,33 @@
+// Multiple inheritance is not possible using classes in Java
+// We should use interface to achieve multiple inheritance like below:
+
+interface Mammals {
+    default void print()
+    {
+        System.out.println("I am mammal");
+    }
+}
+ 
+// Interface 2
+interface MarineAnimals {
+ 
+    default void print()
+    {
+        System.out.println("I am a marine animal");
+    }
+}
+
+public class q1 implements Mammals, MarineAnimals {
+
+    public void print()
+    {
+        System.out.println("I belong to both the categories: Mammals as well as Marine Animals");
+    }
+ 
+
+    public static void main(String args[])
+    {
+        q1 bw = new q1();
+       bw.print();
+    }
+}
